24f899f510535291f64c3d4bc9b212bf425b24c5
ChallengerC <chenzhihong777@outlook.com>
2023-02-12 12:47:09 +0800

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/czh/catkin_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/czh/qingzhou_ws/devel;/opt/ros/melodic'.split(';') if '/home/czh/qingzhou_ws/devel;/opt/ros/melodic' != '' else []
